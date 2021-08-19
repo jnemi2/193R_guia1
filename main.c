2b4f35663fa47242cda6921b134e96aaefc8bfa4
bauti <jnemi@fie.undef.edu.ar>
2021-08-19 14:04:54 -0300

    MOD - Base branch

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include "vector3.h"
+#include <stdlib.h>
+
 
 int main() {
     printf("Hello, World!\n");
-    Vector3 vec = {1, 2, 4};
-    printf("The norm of (%.2f, %.2f, %.2f) is %lf", vec.x, vec.y, vec.z, norm(vec));
+
 
     return 0;
 }
